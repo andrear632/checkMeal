318583bf5392a34c365c3baee77108339a656d9f
andrea mazzitelli <mazzitelli.1835022@studenti.uniroma1.it>
2022-05-20 10:58:29 +0200

    localStorage

---
+++ index.js
@@ -0,0 +1,23 @@
+function loadLocalStorage(){
+    var checked = JSON.parse(localStorage.needs)
+    var div = document.getElementById("checkboxes").getElementsByTagName("input")
+    for(var i = 0; i< div.length; i++){
+        if(checked.includes(div[i].name)){
+            div[i].checked = true
+        }
+    }
+    console.log(checked)
+}
+
+function saveLocalStorage() {
+    var div = document.getElementById("checkboxes").getElementsByTagName("input")
+    var checked = [];
+    for (var i = 0; i < div.length; i++) {
+        if (div[i].checked) {
+            checked.push(div[i].name)
+            console.log(div[i].name)
+        }
+    }
+    localStorage.needs = JSON.stringify(checked)
+    console.log(checked)
+}
