d5d9e9772e5e304fbd641446324bd31eedfaa53f
andrea mazzitelli <mazzitelli.1835022@studenti.uniroma1.it>
2022-05-20 21:23:38 +0200

    alphabetical order not working

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ function loadLocalStorage(){
     if(typeof(localStorage.needs) == "undefined"){
         return
     }
-    var checked = JSON.parse(localStorage.needs)
-    var div = document.getElementById("needs").getElementsByTagName("input")
-    for(var i = 0; i< div.length; i++){
-        if(checked.includes(div[i].name)){
-            div[i].checked = true
+    var checked = JSON.parse(localStorage.needs).sort().reverse()
+    // console.log(checked)
+    var div = document.getElementById("needs")
+    var children = div.children
+   var top = []
+    console.log(div)
+    for(var i = children.length-1; i > 0; i--){
+        console.log(children[i])
+        if(checked.includes(children[i].firstElementChild.firstElementChild.name)){
+            children[i].firstElementChild.firstElementChild.checked = true;
+            var removed = div.removeChild(children[i])
+            top.push(removed)
+            div.insertBefore(removed, div.firstChild)
+            console.log(removed)
         }
     }
-    console.log(checked)
+    console.log(top)
 }
 
 function uncheckAll(){
@@ -96,11 +105,11 @@ function saveLocalStorage() {
     for (var i = 0; i < div.length; i++) {
         if (div[i].checked) {
             checked.push(div[i].name)
-            console.log(div[i].name)
+            // console.log(div[i].name)
         }
     }
     localStorage.needs = JSON.stringify(checked)
-    console.log(checked)
+    // console.log(checked)
 }
 
 
